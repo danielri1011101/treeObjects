dad4f6dfb7e3dde6dc797883e5b898328917628f
Daniel R Barrero R <barreroroserodr@gmail.com>
2025-06-08 12:24:49 -0500

    An array of move edges represents a tree.

--- treeObjects/src/milewski/BoardGame.java
+++ treeObjects/src/milewski/BoardGame.java
@@ -2,4 +2,29 @@ package milewski;
 
 public class BoardGame {
 
+	/**
+	 * Symbol drawn and its coordinates.
+	 * @author daniel
+	 *
+	 */
+	class Move {
+		int symbol;
+		int x;
+		int y;
+	}
+	
+	/**
+	 * The parent field is so that an array of move
+	 * edges easily represents a tree.
+	 * @author daniel
+	 *
+	 */
+	class MEdge {
+		Move move;
+		int parent;
+	}
+	
+	/**
+	 * Now a move tree can simply be an array of move edges.
+	 */
 }
